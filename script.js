99fb41c40d6da8923c5734bd1862762cf13928ef
Sajad <sajadsoorni@gmail.com>
2025-04-16 21:25:59 +0100

    feat(fn-args): illustrate primitive vs object parameter passing
    - Added `checkIn(flightNum, passenger)` to show that reassigning a primitive parameter (flightNum) inside the function doesn’t affect the original variable.
    - Mutated the `passenger.name` property within `checkIn`, demonstrating that objects passed to functions are modified by reference.
    - Introduced `newPassport(person)` to randomize the `paspport` field, highlighting how side effects on object parameters persist outside the function.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 'use strict';
+/*
+// Default Parameters
 
 const bookings = [];
 
@@ -23,3 +25,36 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const sajad = {
+  name: 'Sajad Soornisofla',
+  paspport: 24739479284,
+};
+
+const checkIn = function (flighNum, passenger) {
+  flighNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.paspport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, sajad);
+// console.log(flight);
+// console.log(sajad);
+
+// Is the same as doing...
+// const flighNum = flight;
+// const passenger = sajad;
+
+const newPassport = function (person) {
+  person.paspport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(sajad);
+checkIn(flight, sajad);
